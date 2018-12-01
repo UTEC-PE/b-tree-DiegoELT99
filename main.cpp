566b6fcb620c402cc2adc3aa51876a5f76c6f1c1
DiegoELT99 <diego.linares.t@utec.edu.pe>
2018-12-01 16:18:31 -0500

    Insertion Done

--- main.cpp
+++ main.cpp
@@ -8,13 +8,13 @@ int main(int argc, char *argv[])
 {
 	BTree<unsigned int> t(6);
 
-	t.insert(10); 
-    t.insert(20); 
+	t.insert(1); 
+    t.insert(2); 
     t.insert(5); 
-    t.insert(6); 
-    t.insert(12); 
-    t.insert(30); 
-    t.insert(7); 
+    t.insert(3); 
+    t.insert(4); 
+    t.insert(10); 
+    t.insert(11); 
     t.insert(17); 
 
     t.goThroughSubTree();
